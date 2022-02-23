8f06d4577417c35dc19bdc449e1927b64af624f0
Sam Kennedy <kennedysam168@gmail.com>
2022-02-23 15:57:42 -0500

    deploying to heroku and atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/redux-store',
+  process.env.MONGODB_URI || 'mongodb://localhost/Redux-Store',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
